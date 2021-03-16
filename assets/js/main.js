e7b42d0269f2ac79f6a0ac262243d019a0ba15eb
LOgreee <69425763+LOgreee@users.noreply.github.com>
2021-03-16 15:21:15 +0100

    Update to v.1.16

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,7 +14,8 @@
 			xlarge:  [ '1281px',  '1680px' ],
 			large:   [ '981px',   '1280px' ],
 			medium:  [ '737px',   '980px'  ],
-			small:   [ null,      '736px'  ]
+			small:   [ '401px',      '736px'  ],
+            xsmall:   [ null,      '400px'  ]
 		});
 
 	// Play initial animations on page load.
